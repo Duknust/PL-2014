c81f4c3318f387626c89933f2ffdc2ac0901b15a
Duarte Duarte <duartenuno1992@gmail.com>
2014-03-24 16:33:05 +0000

    Alteração da main para pequenas funçoes
    Adiçao do  --v e do --h

--- TP1 - FLEX/latex.c
+++ TP1 - FLEX/latex.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
-int main(){return 0;};
+#include <string.h>
+#include "latex.h"
 
 void initLaTeX (){
-	
-printf("\\documentclass[11pt, a4paper, oneside]{article}\n");
-
-printf("\\usepackage[portuguese]{babel}\n");
-
-printf("\\usepackage{hyperref}\n");
-printf("\\hypersetup{colorlinks=true, urlcolor=blue, linkcolor=black}\n");
-
-printf("\\usepackage[utf8]{inputenc}\n");
-
-printf("\\usepackage{graphicx}\n");
-printf("\\graphicspath{{./imagens/}}\n");
-
-printf("\\usepackage[labelformat=simple]{caption}\n");
-printf("\\usepackage[labelformat=empty]{subcaption}\n");
-
-printf("\\usepackage{lastpage}\n");
-
-printf("\\usepackage{fancyhdr}\n");
-printf("\\pagestyle{fancy}\n");
-
-
-
-printf("\\usepackage{enumitem}\n");
-
-printf("\\usepackage[algoruled]{algorithm2e}\n");
-
-printf("\\usepackage[usenames,dvipsnames]{color}\n");
-
-printf("\\begin{document}\n\n");
-	
+	printf("\\documentclass[11pt, a4paper, oneside]{article}\n");
+	printf("\\usepackage[portuguese]{babel}\n");
+	printf("\\usepackage{hyperref}\n");
+	printf("\\hypersetup{colorlinks=true, urlcolor=blue, linkcolor=black}\n");
+	printf("\\usepackage[utf8]{inputenc}\n");
+	printf("\\usepackage{graphicx}\n");
+	printf("\\graphicspath{{./imagens/}}\n");
+	printf("\\usepackage[labelformat=simple]{caption}\n");
+	printf("\\usepackage[labelformat=empty]{subcaption}\n");
+	printf("\\usepackage{lastpage}\n");
+	printf("\\usepackage{fancyhdr}\n");
+	printf("\\pagestyle{fancy}\n");
+	printf("\\usepackage{enumitem}\n");
+	printf("\\usepackage{verbatim}\n");
+	printf("\\usepackage[algoruled]{algorithm2e}\n");
+	printf("\\usepackage[usenames,dvipsnames]{color}\n\n\n\n\n\n\n\n\n");
+	printf("\\begin{document}\n");
 }
 
 void tituloL(char * titulo){
@@ -64,26 +51,36 @@ void listafigL(){
 }
 
 void italicoL(char * texto){
-	printf("\\textit{%s}",texto);
+	printf("\\textit{%s",texto);
 }
 
 void negritoL(char * texto){
-	printf("\\textbf{%s}",texto);
+	printf("\\textbf{%s",texto);
 }
 
-void sectionL(char * texto,int nivel){
-	switch nivel{
-	case 1:	printf("\\section{%s}",texto);break;
-	case 2:	printf("\\subsection{%s}",texto);break;
-	case 3:	printf("\\subsubsection{%s}",texto);break;
-	default:break;	
-}	
+void sublinhadoL(char * texto){
+	printf("\\underline{%s",texto);
+}
+
+void nisL(char * texto){
+	printf("\\underline{\\textbf{\\textit{%s",texto);
+}
+
+void sectionL(char * texto,char nivel){
+	if(nivel=='1')
+		printf("\\section{%s}",texto);
+	else if(nivel=='2')
+		printf("\\subsection{%s}",texto);
+	else if(nivel=='3')
+		printf("\\subsubsection{%s}",texto);
+}
 
 void fimL(){
 	printf("\\end{document}\n");
 }
 
+char* tira_ns(char * texto){
+	
+	texto[strlen(texto)-2]='\0';
+	return texto;
 }
-
-
-
--- TP1 - FLEX/latex.h
+++ TP1 - FLEX/latex.h
@@ -1,23 +1,16 @@
+
 void initLaTeX ();
-
 void tituloL(char * titulo);
-
 void autoresL(char * autores);
-
 void fimdecapaL();
-
 void newpageL();
-
 void paragrafoL();
-
 void listafigL();
-
 void tableconL();
-
-void italicoL();
-
-void negritoL();
-
-void sectionL(char * texto,int nivel);
-
+void italicoL(char * texto);
+void negritoL(char * texto);
+void sublinhadoL(char * texto);
+void nisL(char * texto);
+void sectionL(char * texto,char nivel);
 void fimL();
+char *tira_ns(char * texto);
