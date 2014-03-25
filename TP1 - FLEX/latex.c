381d9043c066591d11be3d2b546904f9d41197a0
112 <a61065@gmail.com>
2014-03-25 23:03:53 +0000

    modulos e tabelas

--- TP1 - FLEX/latex.c
+++ TP1 - FLEX/latex.c
@@ -1,89 +1,171 @@
 #include <stdio.h>
-int main(){return 0;};
+#include <stdlib.h>
+#include <string.h>
+#include "latex.h"
 
-void initLaTeX (){
+
+void initLaTeX (FILE * latex_file){
+	fprintf(latex_file,"\\documentclass[11pt, a4paper, oneside]{article}\n");
+	fprintf(latex_file,"\\usepackage[portuguese]{babel}\n");
+	fprintf(latex_file,"\\usepackage{hyperref}\n");
+	fprintf(latex_file,"\\hypersetup{colorlinks=true, urlcolor=blue, linkcolor=black}\n");
+	fprintf(latex_file,"\\usepackage[utf8]{inputenc}\n");
+	fprintf(latex_file,"\\usepackage{graphicx}\n");
+	fprintf(latex_file,"\\graphicspath{{./imagens/}}\n");
+	fprintf(latex_file,"\\usepackage[labelformat=simple]{caption}\n");
+	fprintf(latex_file,"\\usepackage[labelformat=empty]{subcaption}\n");
+	fprintf(latex_file,"\\usepackage{lastpage}\n");
+	fprintf(latex_file,"\\usepackage{fancyhdr}\n");
+	fprintf(latex_file,"\\pagestyle{fancy}\n");
+	fprintf(latex_file,"\\usepackage{enumitem}\n");
+	fprintf(latex_file,"\\usepackage{verbatim}\n");
+	fprintf(latex_file,"\\usepackage[algoruled]{algorithm2e}\n");
+	fprintf(latex_file,"\\usepackage[usenames,dvipsnames]{color}\n\n\n\n\n\n\n\n\n");
+	fprintf(latex_file,"\\begin{document}\n");
+}
+
+char * tituloL(char * titulo){
+	char *temp = (char *) malloc ((10+strlen(titulo)) * sizeof (char));
+	sprintf(temp,"\\title{%s}\n",titulo);
+	return temp;
+}
+
+char * autoresL(char * autores){
+	char *temp = (char *) malloc ((10+strlen(autores)) * sizeof (char));
+	sprintf(temp,"\\author{%s}\n",autores);
+	return temp;
+}
+
+char * fimdecapaL(){
+	return "\\maketitle\n";
+}
+
+char * newpageL(){
+	return "\\newpage\n";
+}
+
+char * paragrafoL(){
+	return "\\\\\n";
+}
+
+char * tableconL(){
+	return "\\tableofcontents\n";
+}
+
+char * listafigL(){
+	return "\\listoffigures\n";
+}
+
+char * italicoL(char * texto){
+	char *temp = (char *) malloc ((10+strlen(texto)) * sizeof (char));
+	sprintf(temp,"\\textit{%s",texto);
+	return temp;
+}
+
+char * negritoL(char * texto){
+	char *temp = (char *) malloc ((10+strlen(texto)) * sizeof (char));
+	sprintf(temp,"\\textbf{%s",texto);
+	return temp;
+}
+
+char * sublinhadoL(char * texto){
+	char *temp = (char *) malloc ((13+strlen(texto)) * sizeof (char));
+	sprintf(temp,"\\underline{%s",texto);
+	return temp;
+}
+
+char * nisL(char * texto){
+	char *temp = (char *) malloc ((35+strlen(texto)) * sizeof (char));
+	sprintf(temp,"\\underline{\\textbf{\\textit{%s",texto);
+	return temp;
+}
+
+char * sectionL(char * texto,char nivel){
 	
-printf("\\documentclass[11pt, a4paper, oneside]{article}\n");
-
-printf("\\usepackage[portuguese]{babel}\n");
-
-printf("\\usepackage{hyperref}\n");
-printf("\\hypersetup{colorlinks=true, urlcolor=blue, linkcolor=black}\n");
-
-printf("\\usepackage[utf8]{inputenc}\n");
-
-printf("\\usepackage{graphicx}\n");
-printf("\\graphicspath{{./imagens/}}\n");
-
-printf("\\usepackage[labelformat=simple]{caption}\n");
-printf("\\usepackage[labelformat=empty]{subcaption}\n");
-
-printf("\\usepackage{lastpage}\n");
-
-printf("\\usepackage{fancyhdr}\n");
-printf("\\pagestyle{fancy}\n");
-
-
-
-printf("\\usepackage{enumitem}\n");
-
-printf("\\usepackage[algoruled]{algorithm2e}\n");
-
-printf("\\usepackage[usenames,dvipsnames]{color}\n");
-
-printf("\\begin{document}\n\n");
+	char *temp = (char *) malloc ((20+strlen(texto)) * sizeof (char));
 	
+	if(nivel=='1')
+		sprintf(temp,"\\section{%s}\n",texto);
+	else if(nivel=='2')
+		sprintf(temp,"\\subsection{%s}\n",texto);
+	else if(nivel=='3')
+		sprintf(temp,"\\subsubsection{%s}\n",texto);
+	
+	return temp;
 }
 
-void tituloL(char * titulo){
-	printf("\\title{%s}\n",titulo);
+char * fimL(){
+	return "\\end{document}\n";
 }
 
-void autoresL(char * autores){
-	printf("\\author{%s}\n",autores);
+char* tira_ns(char * texto){
+	
+	texto[strlen(texto)-2]='\0';
+	return texto;
 }
 
-void fimdecapaL(){
-	printf("\\maketitle\n");
+char * legendaL(char * texto){
+	char *temp = (char *) malloc ((13+strlen(texto)) * sizeof (char));
+	sprintf(temp,"\\caption{%s",texto);
+	return temp;
 }
 
-void newpageL(){
-	printf("\\newpage\n");
+
+char * tabelaL(char * texto){
+	char *temp = (char *) malloc ((50) * sizeof (char));
+	strcat(temp,"\\begin{table}\n\\begin{tabular}{");
+	char * numero = strdup(texto);
+	numero[strlen(texto)-2]='\0';
+	int tamanho=strlen("\\begin{table}\n\\begin{tabular}{");
+	int ncol = atoi(numero);
+
+	//printf("temp=%s...",texto);
+
+	int i;
+	for(i = 0;i<2*ncol;i+=2)
+		{temp[i+tamanho]=texto[strlen(texto)-2];
+		 if(i+2!=2*ncol)temp[i+tamanho+1]='|';}
+
+	strcat(temp,"}\n\\hline\n");
+
+
+	return temp;
 }
 
-void paragrafoL(){
-	printf("\\\\\n");
+char * tabelalinhaL(char * texto){
+	texto[strlen(texto)-1]='\0';
+	char *temp = (char *) malloc ((15+strlen(texto)) * sizeof (char));
+	strcat(temp,texto);
+	strcat(temp,"\\\\ \\hline\n");
+	
+	//printf("_%s_\n",temp);
+	temp[strlen(texto)+14]='\0';
+	
+	
+	return temp;
 }
 
-void tableconL(){
-	printf("\\tableofcontents\n");
+char * fimtabelaL(){
+	return "\\end{table}\n";
 }
 
-void listafigL(){
-	printf("\\listoffigures\n");
-}
+/*\begin{table}
+ 	\begin{tabular}{lll}   
+    \hline
+    ~ & ~ & ~ \\
+    ~ & ~ & ~ \\
+    ~ & ~ & ~ \\
+    ~ & ~ & ~ \\
+    \end{tabular}
+	\end{table}*/
 
-void italicoL(char * texto){
-	printf("\\textit{%s}",texto);
-}
 
-void negritoL(char * texto){
-	printf("\\textbf{%s}",texto);
-}
 
-void sectionL(char * texto,int nivel){
-	switch nivel{
-	case 1:	printf("\\section{%s}",texto);break;
-	case 2:	printf("\\subsection{%s}",texto);break;
-	case 3:	printf("\\subsubsection{%s}",texto);break;
-	default:break;	
-}	
 
-void fimL(){
-	printf("\\end{document}\n");
-}
 
-}
+
+
+
 
 
 
--- TP1 - FLEX/latex.h
+++ TP1 - FLEX/latex.h
@@ -1,23 +1,23 @@
-void initLaTeX ();
 
-void tituloL(char * titulo);
+/*funcoes de LATEX*/
+void initLaTeX (FILE * latex_file);
+char * tituloL(char * titulo);
+char * autoresL(char * autores);
+char * fimdecapaL();
+char * newpageL();
+char * paragrafoL();
+char * listafigL();
+char * tableconL();
+char * italicoL(char * texto);
+char * negritoL(char * texto);
+char * sublinhadoL(char * texto);
+char * nisL(char * texto);
+char * sectionL(char * texto,char nivel);
+char * fimL();
+char *tira_ns(char * texto);
+char * legendaL(char * texto);
+char * tabelaL(char * texto);
+char * tabelalinhaL(char * texto);
+char * fimtabelaL();
 
-void autoresL(char * autores);
 
-void fimdecapaL();
-
-void newpageL();
-
-void paragrafoL();
-
-void listafigL();
-
-void tableconL();
-
-void italicoL();
-
-void negritoL();
-
-void sectionL(char * texto,int nivel);
-
-void fimL();
