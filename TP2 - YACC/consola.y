b88d1c23fae7729c30b60648c6f6fe3ef8e16a27
112 <a61065@gmail.com>
2014-05-06 15:47:49 +0100

    b

--- TP2 - YACC/consola.y
+++ TP2 - YACC/consola.y
@@ -31,7 +31,7 @@ ListaInstrucoes : ListaInstrucoes SEPN Inst
 Inst : LOAD Comando_load ficheiro {printf("LOAD! Ficheiro lido com o nome: %s\n",$3);}
 	 | SAVE ficheiro {$$=$2; printf("SAVE! Ficheiro gravado com o nome: %s\n",$2);}
 	 | RANKING ficheiro
-	 | EXIT {printf("---Até à proxima!---\n"); exit(0);}
+	 | EXIT {printf("---Até à proxima!---\n"); return;/*exit(0);*/}
 	 | LISTING Comando_list
 	 | INFO
 	 ;
@@ -53,9 +53,9 @@ int yyerror(char *s){
 	fprintf(stderr,"%s",s);
 	return -1;
 }
-
+/*
 int main(){
 	yyparse();
 	return 0;
-}
+}*/
 
--- TP2 - YACC/lex.yy.c
+++ TP2 - YACC/lex.yy.c
@@ -46,7 +46,6 @@ typedef int16_t flex_int16_t;
 typedef uint16_t flex_uint16_t;
 typedef int32_t flex_int32_t;
 typedef uint32_t flex_uint32_t;
-typedef uint64_t flex_uint64_t;
 #else
 typedef signed char flex_int8_t;
 typedef short int flex_int16_t;
@@ -54,7 +53,6 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
-#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -84,6 +82,8 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
+
+#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -141,7 +141,15 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k.
+ * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
+ * Ditto for the __ia64__ case accordingly.
+ */
+#define YY_BUF_SIZE 32768
+#else
 #define YY_BUF_SIZE 16384
+#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -153,12 +161,7 @@ typedef unsigned int flex_uint32_t;
 typedef struct yy_buffer_state *YY_BUFFER_STATE;
 #endif
 
-#ifndef YY_TYPEDEF_YY_SIZE_T
-#define YY_TYPEDEF_YY_SIZE_T
-typedef size_t yy_size_t;
-#endif
-
-extern yy_size_t yyleng;
+extern int yyleng;
 
 extern FILE *yyin, *yyout;
 
@@ -184,6 +187,11 @@ extern FILE *yyin, *yyout;
 
 #define unput(c) yyunput( c, (yytext_ptr)  )
 
+#ifndef YY_TYPEDEF_YY_SIZE_T
+#define YY_TYPEDEF_YY_SIZE_T
+typedef size_t yy_size_t;
+#endif
+
 #ifndef YY_STRUCT_YY_BUFFER_STATE
 #define YY_STRUCT_YY_BUFFER_STATE
 struct yy_buffer_state
@@ -201,7 +209,7 @@ struct yy_buffer_state
 	/* Number of characters read into yy_ch_buf, not including EOB
 	 * characters.
 	 */
-	yy_size_t yy_n_chars;
+	int yy_n_chars;
 
 	/* Whether we "own" the buffer - i.e., we know we created it,
 	 * and can realloc() it to grow it, and should free() it to
@@ -271,8 +279,8 @@ static YY_BUFFER_STATE * yy_buffer_stack = 0; /**< Stack as an array. */
 
 /* yy_hold_char holds the character lost when yytext is formed. */
 static char yy_hold_char;
-static yy_size_t yy_n_chars;		/* number of characters read into yy_ch_buf */
-yy_size_t yyleng;
+static int yy_n_chars;		/* number of characters read into yy_ch_buf */
+int yyleng;
 
 /* Points to current character in buffer. */
 static char *yy_c_buf_p = (char *) 0;
@@ -300,7 +308,7 @@ static void yy_init_buffer (YY_BUFFER_STATE b,FILE *file  );
 
 YY_BUFFER_STATE yy_scan_buffer (char *base,yy_size_t size  );
 YY_BUFFER_STATE yy_scan_string (yyconst char *yy_str  );
-YY_BUFFER_STATE yy_scan_bytes (yyconst char *bytes,yy_size_t len  );
+YY_BUFFER_STATE yy_scan_bytes (yyconst char *bytes,int len  );
 
 void *yyalloc (yy_size_t  );
 void *yyrealloc (void *,yy_size_t  );
@@ -355,7 +363,7 @@ static void yy_fatal_error (yyconst char msg[]  );
  */
 #define YY_DO_BEFORE_ACTION \
 	(yytext_ptr) = yy_bp; \
-	yyleng = (yy_size_t) (yy_cp - yy_bp); \
+	yyleng = (size_t) (yy_cp - yy_bp); \
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
@@ -497,7 +505,8 @@ char *yytext;
 #line 1 "consola.l"
 #line 2 "consola.l"
 #include "y.tab.h"	
-#line 501 "lex.yy.c"
+#include "list.h"	
+#line 510 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -536,7 +545,7 @@ FILE *yyget_out (void );
 
 void yyset_out  (FILE * out_str  );
 
-yy_size_t yyget_leng (void );
+int yyget_leng (void );
 
 char *yyget_text (void );
 
@@ -578,7 +587,12 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k */
+#define YY_READ_BUF_SIZE 16384
+#else
 #define YY_READ_BUF_SIZE 8192
+#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -586,7 +600,7 @@ static int input (void );
 /* This used to be an fputs(), but since the string might contain NUL's,
  * we now use fwrite().
  */
-#define ECHO fwrite( yytext, yyleng, 1, yyout )
+#define ECHO do { if (fwrite( yytext, yyleng, 1, yyout )) {} } while (0)
 #endif
 
 /* Gets input and stuffs it into "buf".  number of characters read, or YY_NULL,
@@ -597,7 +611,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		yy_size_t n; \
+		size_t n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( yyin )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -679,9 +693,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 5 "consola.l"
+#line 6 "consola.l"
 
-#line 685 "lex.yy.c"
+#line 699 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -766,79 +780,79 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "consola.l"
+#line 7 "consola.l"
 return LOAD;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "consola.l"
+#line 9 "consola.l"
 return SAVE;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "consola.l"
+#line 11 "consola.l"
 return RANKING;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 12 "consola.l"
+#line 13 "consola.l"
 return EXIT;
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 14 "consola.l"
+#line 15 "consola.l"
 return LISTING;
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 16 "consola.l"
+#line 17 "consola.l"
 return INFO;
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 18 "consola.l"
+#line 19 "consola.l"
 return CONF;
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 20 "consola.l"
+#line 21 "consola.l"
 return DB;
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 22 "consola.l"
+#line 23 "consola.l"
 return RESULT;
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 24 "consola.l"
+#line 25 "consola.l"
 return PROVAS;
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 26 "consola.l"
+#line 27 "consola.l"
 return PARTICIPANTES;
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 28 "consola.l"
+#line 29 "consola.l"
 return PROVA;
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 30 "consola.l"
+#line 31 "consola.l"
 return TORNEIO;
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 32 "consola.l"
+#line 33 "consola.l"
 { yylval.tipoficheiro=strdup(yytext);
 					  printf("estou aqui tex!\n");
 				 	  return ficheiro;}
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 36 "consola.l"
+#line 37 "consola.l"
 { yylval.tiponProva=atoi(yytext);
 					  printf("estou aqui num!\n");
 				 	  return prova;}
@@ -846,29 +860,29 @@ YY_RULE_SETUP
 case 16:
 /* rule 16 can match eol */
 YY_RULE_SETUP
-#line 40 "consola.l"
+#line 41 "consola.l"
 return SEPN;
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 42 "consola.l"
+#line 43 "consola.l"
 ;
 	YY_BREAK
 case YY_STATE_EOF(INITIAL):
-#line 44 "consola.l"
+#line 45 "consola.l"
 return '$';
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 46 "consola.l"
+#line 47 "consola.l"
 ;			
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 48 "consola.l"
+#line 49 "consola.l"
 ECHO;
 	YY_BREAK
-#line 872 "lex.yy.c"
+#line 886 "lex.yy.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -1052,7 +1066,7 @@ static int yy_get_next_buffer (void)
 
 	else
 		{
-			yy_size_t num_to_read =
+			int num_to_read =
 			YY_CURRENT_BUFFER_LVALUE->yy_buf_size - number_to_move - 1;
 
 		while ( num_to_read <= 0 )
@@ -1066,7 +1080,7 @@ static int yy_get_next_buffer (void)
 
 			if ( b->yy_is_our_buffer )
 				{
-				yy_size_t new_size = b->yy_buf_size * 2;
+				int new_size = b->yy_buf_size * 2;
 
 				if ( new_size <= 0 )
 					b->yy_buf_size += b->yy_buf_size / 8;
@@ -1097,7 +1111,7 @@ static int yy_get_next_buffer (void)
 
 		/* Read in more data. */
 		YY_INPUT( (&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move]),
-			(yy_n_chars), num_to_read );
+			(yy_n_chars), (size_t) num_to_read );
 
 		YY_CURRENT_BUFFER_LVALUE->yy_n_chars = (yy_n_chars);
 		}
@@ -1207,7 +1221,7 @@ static int yy_get_next_buffer (void)
 	if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
 		{ /* need to shift things up to make room */
 		/* +2 for EOB chars. */
-		register yy_size_t number_to_move = (yy_n_chars) + 2;
+		register int number_to_move = (yy_n_chars) + 2;
 		register char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
 					YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
 		register char *source =
@@ -1256,7 +1270,7 @@ static int yy_get_next_buffer (void)
 
 		else
 			{ /* need more input */
-			yy_size_t offset = (yy_c_buf_p) - (yytext_ptr);
+			int offset = (yy_c_buf_p) - (yytext_ptr);
 			++(yy_c_buf_p);
 
 			switch ( yy_get_next_buffer(  ) )
@@ -1280,7 +1294,7 @@ static int yy_get_next_buffer (void)
 				case EOB_ACT_END_OF_FILE:
 					{
 					if ( yywrap( ) )
-						return 0;
+						return EOF;
 
 					if ( ! (yy_did_buffer_switch_on_eof) )
 						YY_NEW_FILE;
@@ -1532,7 +1546,7 @@ void yypop_buffer_state (void)
  */
 static void yyensure_buffer_stack (void)
 {
-	yy_size_t num_to_alloc;
+	int num_to_alloc;
     
 	if (!(yy_buffer_stack)) {
 
@@ -1624,16 +1638,17 @@ YY_BUFFER_STATE yy_scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to yylex() will
  * scan from a @e copy of @a bytes.
- * @param bytes the byte buffer to scan
- * @param len the number of bytes in the buffer pointed to by @a bytes.
+ * @param yybytes the byte buffer to scan
+ * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
-YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len )
+YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
 {
 	YY_BUFFER_STATE b;
 	char *buf;
-	yy_size_t n, i;
+	yy_size_t n;
+	int i;
     
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
@@ -1715,7 +1730,7 @@ FILE *yyget_out  (void)
 /** Get the length of the current token.
  * 
  */
-yy_size_t yyget_leng  (void)
+int yyget_leng  (void)
 {
         return yyleng;
 }
@@ -1863,6 +1878,19 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 48 "consola.l"
+#line 49 "consola.l"
 
+
+
+
+int main(int argc, char *argv[])
+{
+    yyparse();
+    yylex();
+    yyin = fopen("f.txt", "r");
+    yyparse();
+    yylex();
+    fclose(yyin);
+    return 1;
+}
 
